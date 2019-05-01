d1428fe1e8638f931030a74d6a00d9c619d0705d
Rebecca Chatikobo <kuchyc>
2019-05-01 08:40:01 +0100

    adding files

--- FirstTestProject/SMECreditScorePage.js
+++ FirstTestProject/SMECreditScorePage.js
@@ -1,1 +1,4 @@
 //this test is to confirm that the Business Credit Score page can be navigated to
+describe("This is my first test") function f() {
+    
+}
--- FirstTestProject/conf.js
+++ FirstTestProject/conf.js
@@ -6,11 +6,12 @@ exports.config = {
     },
     framework: 'jasmine2',
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    //specs: ['spec.js'],
-    //specs: ['FirstTest.js'],
+
     specs: ['SMECreditScorePage.js'],
 
     jasmineNodeOpts: {
         defaultTimeoutInterval: 30000
     }
+
+    //
 };
